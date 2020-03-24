8c5b5eeffe24b0fa5ea7c8c3fdf1dec0b66be621
EliotFerragni <eliot.ferragni@epfl.ch>
2020-03-24 15:57:16 +0100

    -Added possibility to execute a precompiled python script called boot.py if exists at power on -Possibility to enable/disable the build of frozen mpy code and python script stored to flash

--- Micropython_ChibiOS/micropython_chibios.mk
+++ Micropython_ChibiOS/micropython_chibios.mk
@@ -18,8 +18,22 @@ ALLCSRC	+=	$(MPTOP_CHIBIOS)/flash/mp_flash.c \
 ALLINC	+=	$(MPTOP_CHIBIOS)/ \
 			$(MPTOP_CHIBIOS)/mp_flash/
 
+# yes or no to compile and include or not the frozen files described by manifest.py
+MP_FROZEN_PYTHON ?= no
+# yes or no to include or not the python script python_flash_code/py_flash.py in the flash
+# needs to have correctly configured the .ld file
+MP_PYTHON_FLASH_CODE ?= no
+
+ifeq ($(MP_FROZEN_PYTHON),yes)
+	ALLDEFS += -DFROZEN_PYTHON
+endif
+
+ifeq ($(MP_PYTHON_FLASH_CODE),yes)
+	ALLDEFS += -DPYTHON_FLASH_CODE
+endif
+
 preall: 
-	$(MAKE) -C $(MPTOP_CHIBIOS)/ MPTOP=$(MPTOP_FOR_MP_MAKEFILE);
+	$(MAKE) -C $(MPTOP_CHIBIOS)/ MPTOP=$(MPTOP_FOR_MP_MAKEFILE) FROZEN_PYTHON=$(MP_FROZEN_PYTHON) PYTHON_FLASH_CODE=$(MP_PYTHON_FLASH_CODE);
 	$(MAKE) all
 
 CLEAN_RULE_HOOK:
